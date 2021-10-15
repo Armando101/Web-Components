6716a2590091e158e721837cc887765b046852c1
Armando Rivera <rivera.armando997@gmail.com>
2021-10-15 18:40:36 -0500

    Connected Callback and listeng events inside the component

--- 01-Basic/tooltip.js
+++ 01-Basic/tooltip.js
@@ -1,7 +1,25 @@
 class Tooltip extends HTMLElement {
+  tooltipContainer;
   constructor() {
-    super();
-    console.log("It is working");
+    super(); // It's requred to call super method to invoke parent constructor
+  }
+
+  connectedCallback() {
+    const tooltipIcon = document.createElement("span");
+    tooltipIcon.textContent = " (?)";
+    tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
+    tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
+    this.appendChild(tooltipIcon);
+  }
+
+  _showTooltip() {
+    this.tooltipContainer = document.createElement("div");
+    this.tooltipContainer.textContent = "This is the tooltip text";
+    this.appendChild(this.tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.removeChild(this.tooltipContainer);
   }
 }
 
