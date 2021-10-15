a5078f29854bd4efa81a4eb6a4293a08516ffc37
Armando Rivera <rivera.armando997@gmail.com>
2021-10-15 18:49:44 -0500

    Attributes

--- 01-Basic/tooltip.js
+++ 01-Basic/tooltip.js
@@ -1,10 +1,14 @@
 class Tooltip extends HTMLElement {
-  tooltipContainer;
   constructor() {
     super(); // It's requred to call super method to invoke parent constructor
+    this._tooltipContainer;
+    this._tooltipText = "Some dummy tooltip text.";
   }
 
   connectedCallback() {
+    if (this.hasAttribute("text")) {
+      this._tooltipText = this.getAttribute("text");
+    }
     const tooltipIcon = document.createElement("span");
     tooltipIcon.textContent = " (?)";
     tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
@@ -13,13 +17,13 @@ class Tooltip extends HTMLElement {
   }
 
   _showTooltip() {
-    this.tooltipContainer = document.createElement("div");
-    this.tooltipContainer.textContent = "This is the tooltip text";
-    this.appendChild(this.tooltipContainer);
+    this._tooltipContainer = document.createElement("div");
+    this._tooltipContainer.textContent = this._tooltipText;
+    this.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this.tooltipContainer);
+    this.removeChild(this._tooltipContainer);
   }
 }
 
