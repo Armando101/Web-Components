506a7099a2d59757ffdf6bd099585553f71ec075
Armando Rivera <rivera.armando997@gmail.com>
2022-02-14 13:51:21 -0600

    Styling slot inside and outside the shadow DOM

---
+++ 02-Middle/tooltip.js
@@ -0,0 +1,56 @@
+class Tooltip extends HTMLElement {
+  constructor() {
+    super(); // It's requred to call super method to invoke parent constructor
+    this._tooltipContainer;
+    this._tooltipText = "Some dummy tooltip text.";
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = this._htmlTemplate();
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute("text")) {
+      this._tooltipText = this.getAttribute("text");
+    }
+    const tooltipIcon = this.shadowRoot.querySelector("span");
+    tooltipIcon.textContent = " (?)";
+    tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
+    tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = "relative";
+  }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement("div");
+    this._tooltipContainer.textContent = this._tooltipText;
+    this.shadowRoot.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.shadowRoot.removeChild(this._tooltipContainer);
+  }
+
+  _htmlTemplate() {
+    return `
+      <style>
+        div {
+          background-color: black;
+          color: white;
+          position: absolute;
+          z-index: 10;
+        }
+        .highlight {
+          /* This styles have not effect to my slotted element */
+          background-color: orange;
+        }
+        ::slotted(.highlight) {
+          /* This styles are inside my shadow DOM */
+          border: 1px dotted red;
+        }
+      </style>
+      <slot>Some default</slot>
+      <span>(?)</span>`;
+  }
+}
+
+customElements.define("my-tooltip", Tooltip);
