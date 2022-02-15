0e8f338ba4e7f2ff39e3068a25dba6c4e59a79d0
Armando Rivera <rivera.armando997@gmail.com>
2022-02-15 16:15:54 -0600

    WC base

---
+++ 03-Advance/js/modal.js
@@ -0,0 +1,23 @@
+class Modal extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+      <style>
+        #backdrop {
+          position: fixed;
+          top: 0;
+          left: 0;
+          inline-size: 100%;
+          block-size: 100vh;
+          background: rgba(0, 0, 0, 0.75);
+        }
+      </style>
+
+      <div id="backdrop"></div>
+      <div id="modal"></div>
+    `;
+  }
+}
+
+customElements.define("my-modal", Modal);
