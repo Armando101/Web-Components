9fb4b2c2daa443f7c96424661d7f3a77e62463fc
Armando Rivera <rivera.armando997@gmail.com>
2022-02-16 12:51:23 -0600

    Add multiple slots

--- 03-Advance/js/modal.js
+++ 03-Advance/js/modal.js
@@ -62,7 +62,7 @@ class Modal extends HTMLElement {
           padding: 1rem;
         }
 
-        header h1 {
+        ::slotted(h1) {
           font-size: 1.25rem;
         }
 
@@ -85,10 +85,10 @@ class Modal extends HTMLElement {
       <div id="backdrop"></div>
       <div id="modal">
         <header>
-          <h1>Please Confirm</h1>
+          <slot name="title"></slot>
         </header>
         <section id="main">
-          <slot></slot>
+          <slot name="main"></slot>
         </section>
         <section id="actions">
           <button>Cancel</button>
