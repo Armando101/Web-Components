6d48dbb676c868faae6ccfc3f5121a15e0f994ac
Armando Rivera <rivera.armando997@gmail.com>
2022-03-16 15:03:09 -0600

    Build a new component

---
+++ 04-Stencil-Basic/web-components-stencil/src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,12 @@
+import { Component, h } from "@stencil/core";
+
+@Component({
+  tag: 'app-side-drawer',
+  styleUrl: './side-drawer.css',
+  shadow: true
+})
+export class SideDrawer {
+  render() {
+    return <aside><h1>The side drawer</h1></aside>
+  }
+}
