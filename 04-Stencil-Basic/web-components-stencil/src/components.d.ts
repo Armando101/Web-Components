75bbf2f18fcee45645a169a451ab4f63fe22f029
Armando Rivera <rivera.armando997@gmail.com>
2022-03-18 14:35:49 -0600

    Reflect true

--- 04-Stencil-Basic/web-components-stencil/src/components.d.ts
+++ 04-Stencil-Basic/web-components-stencil/src/components.d.ts
@@ -7,6 +7,7 @@
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface AppSideDrawer {
+        "title": string;
     }
     interface MyComponent {
         /**
@@ -43,6 +44,7 @@ declare global {
 }
 declare namespace LocalJSX {
     interface AppSideDrawer {
+        "title"?: string;
     }
     interface MyComponent {
         /**
