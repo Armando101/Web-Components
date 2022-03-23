a8c2c06dc9353e55316e2e97cbe5c8d1353f9df1
Armando Rivera <rivera.armando997@gmail.com>
2022-03-23 14:35:03 -0600

    Manipulate state to navigate between tabs

--- 04-Stencil-Basic/web-components-stencil/src/components/side-drawer/side-drawer.tsx
+++ 04-Stencil-Basic/web-components-stencil/src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from "@stencil/core";
+import { Component, h, Prop, State } from "@stencil/core";
 
 @Component({
   tag: 'app-side-drawer',
@@ -6,6 +6,7 @@ import { Component, h, Prop } from "@stencil/core";
   shadow: true
 })
 export class SideDrawer {
+  @State() showContactInfo = false;
   @Prop({ reflect: true }) title: string;
   @Prop({ reflect: true, mutable: true }) open: boolean;
 
@@ -13,26 +14,38 @@ export class SideDrawer {
     this.open = false;
   }
 
+  onContentChange(content: string) {
+   this.showContactInfo = (content === 'contact');
+  }
+
   render() {
-    // let content = null;
-    // if(this.open) {
-    //   content = (
-    //     <aside>
-    //       <header><h1>{this.title}</h1></header>
-    //       <main>
-    //         <slot/>
-    //       </main>
-    //     </aside>
-    //   )
-    // }
-    // return content;
+    let mainContent = <slot/>
+    if (this.showContactInfo) {
+      mainContent= (
+        <div class="contactInformation">
+          <h2>Contact information</h2>
+          <p>You can reach us via phone or email</p>
+          <ul>
+            <li>
+              Phone: 12312313
+            </li>
+            <li>E mail <a href="mailto:rivera.armando997@gmail.com">rivera.armando997@gmail.com</a></li>
+          </ul>
+        </div>  
+      )
+    }
+
     return <aside>
             <header>
               <h1>{this.title}</h1>
               <button onClick={this.onCloseDrawer.bind(this)}>x</button>
             </header>
+            <section class="tabs" id="tabs">
+              <button class={!this.showContactInfo && "active"} onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
+              <button class={this.showContactInfo && "active"} onClick={this.onContentChange.bind(this, 'contact')}>Content</button>
+            </section>
             <main>
-              <slot/>
+              {mainContent}
             </main>
           </aside>
   }
