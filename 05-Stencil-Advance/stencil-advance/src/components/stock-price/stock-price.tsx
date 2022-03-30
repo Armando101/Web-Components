59ca8daf4d12f67fcfff28e6e9cff8c38d10d919
Armando Rivera <rivera.armando997@gmail.com>
2022-03-30 15:34:42 -0600

    feat: stock price component, fetch to API

--- 05-Stencil-Advance/stencil-advance/src/components/stock-price/stock-price.tsx
+++ 05-Stencil-Advance/stencil-advance/src/components/stock-price/stock-price.tsx
@@ -1,4 +1,5 @@
-import { Component, h } from "@stencil/core";
+import { Component, Element, h, State } from "@stencil/core";
+import { AV_API_KEY } from "../../global/global";
 
 @Component({
   tag: 'app-stock-price',
@@ -6,20 +7,33 @@ import { Component, h } from "@stencil/core";
   shadow: true
 })
 export class StockPrice {
+  @Element() el: HTMLElement;
+  @State() price: number;
 
   onFetchStockPrice(event: Event) {
+    const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
     event.preventDefault();
-    console.log(event);
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`)
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      const response = res["Global Quote"];
+      this.price = Number(response["05. price"]);
+    })
+    .catch(err => {
+      console.error(err);
+    })
   }
 
   render() {
     return [
-      <form onSubmit={this.onFetchStockPrice}>
+      <form onSubmit={this.onFetchStockPrice.bind(this)}>
         <input id="stock-symbol" type="text" />
         <button type="submit">Fetch</button>
       </form>,
       <div>
-        <p>Price: {0}</p>
+        <p>Price: ${this.price || 0}</p>
       </div>
     ];
   }
